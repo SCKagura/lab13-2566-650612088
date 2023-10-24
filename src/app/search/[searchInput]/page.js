e93732c7d4d6baa5e2db2f4af49f17e6165f3a05
SCKboss <=>
2023-10-24 11:39:02 +0700

    lab13

--- src/app/search/[searchInput]/page.js
+++ src/app/search/[searchInput]/page.js
@@ -3,7 +3,11 @@
 import { MovieRow } from "@/components/MovieRow";
 import { movieDB } from "@/libs/movieDB";
 
-export default function SearchResultPage() {
+export default function SearchResultPage({ params }) {
+  const searchInput = params.searchInput.replaceAll("%20", " ");
+  const filteredMovies = movieDB.filter((movie) =>
+    movie.title.toLocaleLowerCase().includes(searchInput.toLocaleLowerCase())
+  );
   //tip1 : before filtering movie, replace all "%20" with " " (space) in the input
   // const processedSearchInput = ...
 
@@ -14,32 +18,18 @@ export default function SearchResultPage() {
   tip3 : To implement case insensitive searching, use "toLocaleLowerCase" string method
   to convert movie title and searchInput to lower case 
   const filteredMovies = movieDB.filter((movie) =>
-    you code here... */
-  const searchInput = params.searchInput;
-  const processedSearchInput = searchInput.replaceAll("%20", " ");
-  const filteredMovies = movieDB.filter((movie) =>
-    movie.title
-      .toLocaleLowerCase()
-      .includes(processedSearchInput.toLocaleLowerCase())
+    you code here...
   );
-
-  const movieLength = filteredMovies.length;
+  */
 
   return (
     <div>
       <p className="fw-bold fs-4 text-center my-0">
-        Searching &quot; {processedSearchInput} &quot;
+        Searching &quot; {searchInput} &quot;
       </p>
-      <p className="fw-bold fs-4 text-center">Found {movieLength} result(s)</p>
-      {filteredMovies.map((movie, i) => (
-        <MovieRow
-          key={movie.id}
-          id={movie.id}
-          title={movie.title}
-          detail={movie.detail}
-          rating={movie.rating}
-          number={i + 1}
-        />
+      <p className="fw-bold fs-4 text-center">Found {filteredMovies.length} result(s)</p>
+      {filteredMovies.map((movie) => (
+        <MovieRow key={movie.id} id={movie.id} title={movie.title} rating={movie.rating} number={movie.id} />
       ))}
     </div>
   );
